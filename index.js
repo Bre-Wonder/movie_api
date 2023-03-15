ded2d1dcfb4a18c2a792bdff2b9d5a75e48dc1d9
Bre-Wonder <117392929+Bre-Wonder@users.noreply.github.com>
2023-03-14 19:55:53 -0700

    added movie names

---
+++ index.js
@@ -0,0 +1,39 @@
+const express = require('express');
+const app = express ();
+
+let topMovies = [
+    {
+      title: 'Hunt for the Wilderpeople'
+    },
+    {
+        title: 'Roman Holiday'
+    },
+    {
+        title: 'The Parent Trap'
+    },
+    {
+        title: 'Sisterhood of the Traveling Pants'
+    },
+    {
+        title: 'Emperor New Groove'
+    },
+    {
+        title: 'The Little Mermaid'
+    },
+    {
+        title: 'Ocean\'s Eleven'
+    },
+    {
+        title: 'Crazy Rich Asians'
+    },
+    {
+        title: 'La La Land'
+    },
+    {
+        title: 'Romancing the Stone'
+    },
+    {
+        title: 'How to Loose a A Guy in Ten Days'
+    }
+
+]
