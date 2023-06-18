4e03d8a5f6593c0c1d33cd12a699547dcfa120e8
Bre-Wonder <117392929+Bre-Wonder@users.noreply.github.com>
2023-06-17 18:06:55 -0700

    changed orgins back to all

--- index.js
+++ index.js
@@ -21,21 +21,7 @@ app.use(bodyParser.urlencoded({ extended: true }));
 
 let auth = require('./auth')(app);
 const cors = require('cors');
-
-let allowedOrigins = ['http://localhost:8080', 'http://testsite.com'];
-
-app.use(cors({
-    origin: (origin, callback) => {
-        if(!origin) return callback(null, true);
-        if(allowedOrigins.indexOf(origin) === -1) {
-          let message = 'The CORS policy for this application does not allow access from origin ' +
-          origin;
-          return callback(new Error(message ), false);
-        }
-        return callback(null, true);
-    }
-}));
-
+app.use(cors());
 const passport = require('passport');
 require('./passport');
 
