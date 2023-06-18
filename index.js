ed1232863ab835c05b4f481406a635449350eb87
Bre-Wonder <117392929+Bre-Wonder@users.noreply.github.com>
2023-06-17 18:05:02 -0700

    allowed only certain origins access

--- index.js
+++ index.js
@@ -21,7 +21,21 @@ app.use(bodyParser.urlencoded({ extended: true }));
 
 let auth = require('./auth')(app);
 const cors = require('cors');
-app.use(cors());
+
+let allowedOrigins = ['http://localhost:8080', 'http://testsite.com'];
+
+app.use(cors({
+    origin: (origin, callback) => {
+        if(!origin) return callback(null, true);
+        if(allowedOrigins.indexOf(origin) === -1) {
+          let message = 'The CORS policy for this application does not allow access from origin ' +
+          origin;
+          return callback(new Error(message ), false);
+        }
+        return callback(null, true);
+    }
+}));
+
 const passport = require('passport');
 require('./passport');
 
