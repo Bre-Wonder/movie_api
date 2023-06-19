f1ae5af17b49d9528280a1c7138ed3160cbbf27d
Bre-Wonder <117392929+Bre-Wonder@users.noreply.github.com>
2023-06-18 20:38:01 -0700

    updated port

--- index.js
+++ index.js
@@ -253,6 +253,7 @@ app.use((err, req, res, next) => {
     res.status(500).send('Whoops, something went wrong!');
 });
 
-app.listen(8080, () => {
-    console.log ('Your movies are being spied on');
-});
+const port = process.env.PORT || 8080;
+app.listen(port, '0.0.0.0',() => {
+  console.log('Listening on Port ' + port);
+})
